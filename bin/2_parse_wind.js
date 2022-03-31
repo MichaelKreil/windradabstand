13d22e5805d788be87d23b94c1689b879e41b3a9
Michael Kreil <github@michael-kreil.de>
2022-03-31 13:26:15 +0200

    find bundesland

--- bin/2_parse_wind.js
+++ bin/2_parse_wind.js
@@ -3,6 +3,7 @@
 
 
 const fs = require('fs');
+const turf = require('@turf/turf');
 const AdmZip = require('adm-zip');
 const { XMLParser } = require('fast-xml-parser');
 const config = require('../config.js');
@@ -14,6 +15,9 @@ start()
 async function start() {
 	let zip = new AdmZip(config.getFile.src('Gesamtdatenexport_20220330__f3b8b76f16b2426fafb59f8c747a8406.zip'));
 	let zipEntries = zip.getEntries();
+
+	console.log('load Bundesländer')
+	let findBundesland = BundeslandFinder();
 
 	console.log('load Katalogwerte')
 	let translateKeys = KeyTranslator(zipEntries.find(e => e.entryName === 'Katalogwerte.xml'))
@@ -30,6 +34,11 @@ async function start() {
 	windEntries.forEach((windEntry, i) => {
 		if (i % 100 === 0) process.stdout.write('\r'+(100*i/windEntries.length).toFixed(1)+'%');
 		translateKeys(windEntry);
+
+		windEntry.Hoehe = (windEntry.Nabenhoehe + windEntry.Rotordurchmesser/2);
+		windEntry.Bundesland = findBundesland(windEntry.Laengengrad, windEntry.Breitengrad).properties;
+		if (!windEntry.Bundesland) return;
+		windEntry.MinDistanz = getMinDistance(windEntry.Bundesland.AGS, windEntry.Hoehe)
 
 		wind.push({
 			type: 'Feature',
@@ -71,7 +80,7 @@ function KeyTranslator(zipEntry) {
 		}
 	})
 
-	let func = obj => {
+	return obj => {
 		Object.keys(obj).forEach(key => {
 			//console.log(key);
 			let result = keys.get(key);
@@ -85,20 +94,74 @@ function KeyTranslator(zipEntry) {
 
 			if (result) {
 				let v = result.get(obj[key]);
-				if (!v) {
-					console.log(`obj.${key} = `+JSON.stringify(obj[key]))
-					func.errors = true;
-				}
+				if (!v) console.log(`obj.${key} = `+JSON.stringify(obj[key]))
 				obj[key] = v;
 				return
 			}
 
-			func.errors = true;
 			console.log('unknown', key, obj[key])
 			return;
 		})
 		return obj;
 	}
+}
 
-	return func;
+function getMinDistance(ags, hoehe) {
+	switch (ags) {
+		case '01': /* Schleswig-Holstein */ return false;
+		case '02': /* Hamburg */ return 300;
+		case '03': /* Niedersachsen */ return 400;
+		case '04': /* Bremen */ return 450;
+		case '05': /* Nordrhein-Westfalen */ return 1000;
+		case '06': /* Hessen */ return 1000;
+		case '07': /* Rheinland-Pfalz */ return 500;
+		case '08': /* Baden-Württemberg */ return false;
+		case '09': /* Bayern */ return 10*hoehe;
+		case '10': /* Saarland */ return false;
+		case '11': /* Berlin */ return false;
+		case '12': /* Brandenburg */ return 1000;
+		case '13': /* Mecklenburg-Vorpommern */ return 800;
+		case '14': /* Sachsen */ return false;
+		case '15': /* Sachsen-Anhalt */ return false;
+		case '16': /* Thüringen */ return false;
+		default:
+			throw Error('unknown ags '+ags)
+	}
+}
+
+function BundeslandFinder() {
+	let gridScale = 10;
+	let grid = new Map();
+
+	let bundeslaender = fs.readFileSync(config.getFile.src('VG250_Bundeslaender.geojson'));
+	bundeslaender = JSON.parse(bundeslaender).features;
+	bundeslaender.forEach(bundesland => {
+		turf.flatten(bundesland).features.forEach(polygon => {
+			polygon.bbox = turf.bbox(polygon);
+			let x0 = Math.floor(polygon.bbox[0]*gridScale);
+			let x1 = Math.ceil( polygon.bbox[2]*gridScale);
+			let y0 = Math.floor(polygon.bbox[1]*gridScale);
+			let y1 = Math.ceil( polygon.bbox[3]*gridScale);
+			for (let y = y0; y < y1; y++) {
+				for (let x = x0; x < x1; x++) {
+					let cellPolygon = turf.bboxPolygon([x0/gridScale, y0/gridScale, x1/gridScale, y1/gridScale])
+					if (!turf.booleanIntersects(cellPolygon, polygon)) continue;
+					let key = x+'_'+y;
+					if (!grid.has(key)) grid.set(key, []);
+					grid.get(key).push(polygon);
+				}
+			}
+		})
+	})
+	return (lng,lat) => {
+		let point = [lng,lat]
+		let x = Math.floor(lng*gridScale);
+		let y = Math.floor(lat*gridScale);
+		let key = x+'_'+y;
+		if (!grid.has(key)) return false;
+		let result = grid.get(key).filter(polygon => turf.booleanPointInPolygon(point, polygon));
+		if (result.length === 1) return result[0];
+		if (result.length === 0) return false;
+		throw Error('polygons overlapping?');
+	}
 }
