3e6f5834b317d8fed6489c05893961ef03655b3e
Michael Kreil <github@michael-kreil.de>
2022-04-21 14:33:13 +0200

    download when not downloaded

--- bin/1_wind-1_download.js
+++ bin/1_wind-1_download.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 'use strict'
 
-const { fetch, download } = require('../lib/helper.js');
+const fs = require('fs');
 const cheerio = require('cheerio');
+const { fetch, download } = require('../lib/helper.js');
 const config = require('../config.js');
 
 (async () => {
    let html = await fetch('https://www.marktstammdatenregister.de/MaStR/Datendownload');
    let $ = cheerio.load(html);
    let downloadUrl = $('a[title=Download]').attr('href');
-   await download(downloadUrl, config.getFilename.wind('marktstammdatenregister.zip'), true);
-   console.log('\nFinished')
+	let filename = config.getFilename.wind('marktstammdatenregister.zip');
+	if (fs.existsSync(filename)) return;
+   await download(downloadUrl, filename, true);
 })()
