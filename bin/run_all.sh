8b1fd43728a4682d778072d4a0ff53870e80144d
Michael Kreil <github@michael-kreil.de>
2022-04-22 14:30:10 +0200

    all scripts

--- bin/run_all.sh
+++ bin/run_all.sh
@@ -6,4 +6,7 @@ node 1_wind-1_download.js
 node 1_wind-2_parse.js
 node 2_alkis-1_scrape.js
 node 2_alkis-2_gebaeude.js
-node 2_alkis-3_flaechen.js
+node 2_alkis-3_siedlungsflaeche.js
+node 2_alkis-4_grenze_flaeche.js
+node 2_alkis-5_verkehrslinie.js
+node 2_alkis-6_versorgungslinie.js
