820680d310ed38ec412b72f1678e9f8d6a2c40e1
Michael Kreil <github@michael-kreil.de>
2022-04-24 13:13:42 +0200

    update

--- bin/run_all.sh
+++ bin/run_all.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-node 1_wind-1_download.js
+#node 1_wind-1_download.js
 node 1_wind-2_parse.js
 node 2_alkis-1_scrape.js
 node 2_alkis-2_gebaeude.js
@@ -10,3 +10,4 @@ node 2_alkis-3_siedlungsflaeche.js
 node 2_alkis-4_grenze_flaeche.js
 node 2_alkis-5_verkehrslinie.js
 node 2_alkis-6_versorgungslinie.js
+# node 5_generate_frontend.js
