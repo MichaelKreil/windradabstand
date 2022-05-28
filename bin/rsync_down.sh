89125f415df97ce5044847d1099a1664debd21e4
Michael Kreil <github@michael-kreil.de>
2022-05-28 22:28:29 +0200

    use faster hash

--- bin/rsync_down.sh
+++ bin/rsync_down.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")"
 set -ex
 
-rsync -azhtPe ssh --info=progress2 --exclude='cache/' root@168.119.98.135:/root/projects/windradabstand/data/ /Users/michaelkreil/Projekte/privat/ZSHH/windradabstand/data/ --delete-after
+rsync -azhtPe ssh --cc=xxh3 --info=progress2 --exclude='cache/' root@168.119.98.135:/root/projects/windradabstand/data/ /Users/michaelkreil/Projekte/privat/ZSHH/windradabstand/data/ --delete-after
