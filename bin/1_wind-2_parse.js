5461e4bef735d16684608b3883af12a2b7652ef2
Michael Kreil <github@michael-kreil.de>
2022-05-29 17:44:37 +0200

    fix turbine order and add group indexes

--- bin/1_wind-2_parse.js
+++ bin/1_wind-2_parse.js
@@ -64,15 +64,14 @@ async function start() {
 	console.log();
 	windEntries = Array.from(map.values());
 
+	console.log('TSP sort and add local group indexes');
+	windEntries = await tspSort(windEntries);
+
 	fs.writeFileSync(config.getFilename.wind('wind.debug.geojson'), JSON.stringify(turf.featureCollection(debugGeoJSON)));
 	fs.writeFileSync(config.getFilename.wind('wind.json'), JSON.stringify(windEntries));
 
-	console.log('height distribution:');
-	// calculate height statistics
-	let hoehen = windEntries.map(w => w.hoehe);
-	hoehen.sort((a,b) => a-b);
-	let numbers = [...Array(100).keys()];
-	console.log(numbers.map(i => hoehen[Math.round((hoehen.length-1)*(i+0.5)/100)]).join(','))
+	// List heights
+	//console.log(windEntries.map(w => [w.Nabenhoehe, w.Rotordurchmesser].join('\t')).join('\n'));
 }
 
 function loadZipEntry(zipEntry) {
@@ -134,4 +133,108 @@ function floatyEnough(value) {
 	value = value*100;
 	value = Math.abs(Math.round(value) - value);
 	return value < 1e-8
+}
+
+async function tspSort(entries) {
+	const maxDInGroup = 0.03;
+
+	const Delaunator = (await import('delaunator')).default;
+
+	const c = Math.cos(51*Math.PI/180);
+	const points0 = entries.map((entry,index) => {
+		let point = [ entry.Laengengrad*c, entry.Breitengrad ];
+		return Object.assign(point, { index, group:index, count:0, entry, neighbours:[] });
+	});
+
+	let points = points0.slice();
+	let segmentKeys = new Set();
+	let step = 0;
+	let groupsSaved = false;
+
+	do {
+		if (step++ > 100) throw Error();
+
+		const { halfedges, hull, triangles } = Delaunator.from(points);
+
+		let edges = [];
+		for (let i = 0; i < halfedges.length; i++) {
+			const j = halfedges[i];
+			if (j > i) addEdge(triangles[i], triangles[j]);
+		}
+		for (let i = 1; i < hull.length; i++) addEdge(hull[i-1], hull[i]);
+		addEdge(hull[hull.length-1], hull[0]);
+
+		function addEdge(i0, i1) {
+			if (i0 === i1) return;
+			let p0 = points[i0];
+			let p1 = points[i1];
+			let dx = p0[0] - p1[0];
+			let dy = p0[1] - p1[1];
+			let d = dx*dx + dy*dy;
+			edges.push({p0, p1, d});
+		}
+
+		edges.sort((a,b) => a.d - b.d);
+
+		let maxIndex = Math.round(edges.length/5+2);
+
+		edges.slice(0, maxIndex).forEach(edge => {
+			let {p0, p1} = edge;
+
+			if (!groupsSaved && (edge.d > maxDInGroup)) {
+				let knownGroups = new Map();
+				points0.forEach(p => {
+					let groupId = p.group, groupIndex;
+					if (knownGroups.has(groupId)) {
+						groupIndex = knownGroups.get(groupId);
+					} else {
+						groupIndex = knownGroups.size;
+						knownGroups.set(groupId, groupIndex);
+					}
+					p.entry.groupIndex = groupIndex;
+				})
+				console.log('   found',knownGroups.size,'groups');
+				groupsSaved = true;
+			}
+
+			if (p0.count >= 2) return;
+			if (p1.count >= 2) return;
+			if (p0.group === p1.group) return;
+			let i0 = p0.index;
+			let i1 = p1.index;
+			let key = (i0 < i1) ? i0+'_'+i1 : i1+'_'+i0;
+			if (segmentKeys.has(key)) return;
+			segmentKeys.add(key);
+
+			p0.count++; p0.neighbours.push(p1);
+			p1.count++; p1.neighbours.push(p0);
+
+			let p2, newGroup = p0.group;
+			while (true) {
+				p1.group = newGroup;
+				p2 = (p1.neighbours[0] === p0) ? p1.neighbours[1] : p1.neighbours[0];
+				if (!p2) break;
+				p0 = p1;
+				p1 = p2;
+			}
+		})
+
+		points = points.filter(p => p.count < 2);
+	} while (points.length > 2)
+
+	if (!groupsSaved) throw Error();
+
+	let pStart = points[0];
+	let pEnd   = points[1];
+	
+	let p0 = pEnd, p1 = pStart, p2, path = [pStart];
+	while (true) {
+		p2 = (p1.neighbours[0] === p0) ? p1.neighbours[1] : p1.neighbours[0];
+		path.push(p2);
+		if (p2 === pEnd) break;
+		p0 = p1;
+		p1 = p2;
+	}
+
+	return path.map(p => p.entry);
 }
