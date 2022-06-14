834c71dc67841e81a47df0932fe9591539814bb0
Michael Kreil <github@michael-kreil.de>
2022-06-14 13:45:27 +0200

    improve schutzgebiete

--- bin/4_process-3_grenze_flaeche.js
+++ bin/4_process-3_grenze_flaeche.js
@@ -3,36 +3,62 @@
 
 
 
+const config = require('../config.js');
 const { processAlkis } = require('../lib/geohelper.js');
 
 
+start()
 
-let lookup = initLookup();
+async function start() {
+	let lookup = initLookup();
 
-processAlkis({
-	slug:'grenze_flaeche',
-	ruleTypes:'biosphaere,ffhabitat,landschaftsschutz,naturpark,nationalpark,naturschutz,vogelschutz,naturdenkmal'.split(','),
-	cbFeature:feature => {
-		feature.properties.type = lookup.get(feature.properties.klasse);
-		if (feature.properties.type === undefined) throw Error(`Klasse "${feature.properties.klasse}" unbekannt`);
-		return feature.properties.type;
+	await processAlkis({
+		slug:'grenze_flaeche',
+		ruleTypes:'naturdenkmal,biosphaere,ffhabitat,landschaftsschutz,nationalpark,naturpark,naturschutz,vogelschutz'.split(','),
+		cbFeature:feature => {
+			feature.properties.type = lookup.get(feature.properties.klasse);
+			if (feature.properties.type === undefined) throw Error(`Klasse "${feature.properties.klasse}" unbekannt`);
+			return feature.properties.type;
+		}
+	})
+
+	let types = 'biosphaere,ffhabitat,landschaftsschutz,nationalpark,naturpark,naturschutz,vogelschutz'.split(',');
+	for (let type of types) {
+		await processAlkis({
+			slug:'grenze_flaeche',
+			slugOut:type,
+			ruleTypes:[type],
+			filenameIn: config.getFilename.schutzgebiete(type+'.geojsonl'),
+			cbFeature:feature => {
+				feature.properties.type = type;
+			}
+		})
 	}
-})
+}
+
+
 
 function initLookup() {
 	let lookup = new Map();
-	lookup.set('Biosphärenreservat', 'biosphaere');
-	lookup.set('Flora-Fauna-Habitat-Gebiet', 'ffhabitat');
-	lookup.set('Landschaftsschutzgebiet', 'landschaftsschutz');
-	lookup.set('Naturpark', 'naturpark');
-	lookup.set('Nationalpark', 'nationalpark');
-	lookup.set('Naturschutzgebiet', 'naturschutz');
-	lookup.set('Vogelschutzgebiet', 'vogelschutz');
-	lookup.set('Naturdenkmal', 'naturdenkmal');
 	[
+		'Biosphärenreservat',
+		'Flora-Fauna-Habitat-Gebiet',
+		'Landschaftsschutzgebiet',
+		'Naturpark',
+		'Nationalpark',
+		'Naturschutzgebiet',
+		'Vogelschutzgebiet',
 		'Geschützter Landschaftsbestandteil',
 		'Truppenübungsplatz, Standortübungsplatz',
 		'Wasserschutzgebiet',
 	].forEach(label => lookup.set(label, false))
+	lookup.set('Naturdenkmal', 'naturdenkmal');
+	//lookup.set('Biosphärenreservat', 'biosphaere');
+	//lookup.set('Flora-Fauna-Habitat-Gebiet', 'ffhabitat');
+	//lookup.set('Landschaftsschutzgebiet', 'landschaftsschutz');
+	//lookup.set('Naturpark', 'naturpark');
+	//lookup.set('Nationalpark', 'nationalpark');
+	//lookup.set('Naturschutzgebiet', 'naturschutz');
+	//lookup.set('Vogelschutzgebiet', 'vogelschutz');
 	return lookup
 }
