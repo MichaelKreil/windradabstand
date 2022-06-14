d4de620fac18e5608ad8798524270a9093d27be3
Michael Kreil <github@michael-kreil.de>
2022-06-14 11:40:55 +0200

    rename schutzgebiete

--- bin/3_download_schutzgebiete.sh
+++ bin/3_download_schutzgebiete.sh
@@ -2,11 +2,12 @@
 cd "$(dirname "$0")"
 set -ex
 
+ogr2ogr -overwrite ../data/3_schutzgebiete/biosphaere.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Biosphaerenreservate
+ogr2ogr -overwrite ../data/3_schutzgebiete/ffhabitat.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Fauna_Flora_Habitat_Gebiete
+ogr2ogr -overwrite ../data/3_schutzgebiete/landschaftsschutz.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Landschaftsschutzgebiete
+ogr2ogr -overwrite ../data/3_schutzgebiete/nationalpark.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Nationalparke
+ogr2ogr -overwrite ../data/3_schutzgebiete/naturpark.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Naturparke
+ogr2ogr -overwrite ../data/3_schutzgebiete/naturschutz.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Naturschutzgebiete
+ogr2ogr -overwrite ../data/3_schutzgebiete/vogelschutz.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Vogelschutzgebiete
+
 ogr2ogr -overwrite ../data/3_schutzgebiete/nationale_naturmonumente.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Nationale_Naturmonumente
-ogr2ogr -overwrite ../data/3_schutzgebiete/fauna_flora_habitat_gebiete.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Fauna_Flora_Habitat_Gebiete
-ogr2ogr -overwrite ../data/3_schutzgebiete/vogelschutzgebiete.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Vogelschutzgebiete
-ogr2ogr -overwrite ../data/3_schutzgebiete/biosphaerenreservate.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Biosphaerenreservate
-ogr2ogr -overwrite ../data/3_schutzgebiete/nationalparke.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Nationalparke
-ogr2ogr -overwrite ../data/3_schutzgebiete/naturparke.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Naturparke
-ogr2ogr -overwrite ../data/3_schutzgebiete/naturschutzgebiete.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Naturschutzgebiete
-ogr2ogr -overwrite ../data/3_schutzgebiete/landschaftsschutzgebiete.geojsonl WFS:"https://geodienste.bfn.de/ogc/wfs/schutzgebiet?REQUEST=GetCapabilities&SERVICE=WFS&VERSION=2.0.0" Landschaftsschutzgebiete
