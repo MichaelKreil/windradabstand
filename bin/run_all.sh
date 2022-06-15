a614e28da5d18a9bb933a9c38c9ced70b9499e6c
Michael Kreil <github@michael-kreil.de>
2022-06-15 09:55:53 +0200

    update run all script

--- bin/run_all.sh
+++ bin/run_all.sh
@@ -2,13 +2,9 @@
 cd "$(dirname "$0")"
 set -ex
 
-./1_wind-1_download.js
-./1_wind-2_parse.js
-./2_alkis-1_scrape.js
-./2_alkis-2_merge.js
-./2_alkis-3_gebaeude.js
-./2_alkis-4_siedlungsflaeche.js
-./2_alkis-5_grenze_flaeche.js
-./2_alkis-6_verkehrslinie.js
-./2_alkis-7_versorgungslinie.js
-./5_generate_frontend.js
+rm -r ../data/4_map_result
+rm -r ../data/6_buffered_geometry
+rm -r ../data/helper
+
+./4_process-all.sh
+./6_calc_buffered_geometry.js
