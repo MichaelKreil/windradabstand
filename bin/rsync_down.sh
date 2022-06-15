30fe27038107b15c4f03ff0281900dd4661b290c
Michael Kreil <github@michael-kreil.de>
2022-06-15 13:20:55 +0200

    speed up

--- bin/rsync_down.sh
+++ bin/rsync_down.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")"
 set -ex
 
-rsync -azvhtPSe "ssh -o Compression=no" --zc=lz4 --zl=1 --inplace --exclude='cache/' --exclude='*.tar' root@168.119.98.135:/root/projects/windradabstand/data/ /Users/michaelkreil/Projekte/privat/ZSHH/windradabstand/data/ --delete-after
+rsync -azxvhtPSe "ssh -o Compression=no" -B 65536 --inplace --exclude='cache/' --exclude='*.tar' root@168.119.98.135:/root/projects/windradabstand/data/ /Users/michaelkreil/Projekte/privat/ZSHH/windradabstand/data/ --delete-after
