3cfbe08ba89c68699c68e54a3a5176fdb2948508
Michael Kreil <github@michael-kreil.de>
2022-06-18 22:33:33 +0200

    formatieren

--- bin/4_process-all.sh
+++ bin/4_process-all.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 
-ls -1 4_process-*.js | parallel -j 1 node {}
+node 4_process-1_siedlungsflaeche.js
+node 4_process-2_gebaeude.js
+node 4_process-3_grenze_flaeche.js
+node 4_process-4_verkehrslinie.js
+node 4_process-5_versorgungslinie.js
