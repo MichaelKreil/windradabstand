7a29a3579d4916d3336560f48275b1601946e254
Michael Kreil <github@michael-kreil.de>
2022-06-19 18:40:18 +0200

    add map rendering to run_all

--- bin/run_all.sh
+++ bin/run_all.sh
@@ -5,6 +5,8 @@ set -ex
 rm -r ../data/4_map_result
 rm -r ../data/6_buffered_geometry
 rm -r ../data/helper
+rm -r ../docs/tiles
 
 ./4_process-all.sh
 ./6_buffered_geometry-1_calc.js
+./7_render_map-all.sh
