3f10b72dc30604fdbbe33c2081c5dbef4a9cdcda
Michael Kreil <github@michael-kreil.de>
2022-06-19 17:26:16 +0200

    line for downloading tiles

--- bin/rsync_down.sh
+++ bin/rsync_down.sh
@@ -3,3 +3,4 @@ cd "$(dirname "$0")"
 set -ex
 
 rsync -azxvhtPSe "ssh -o Compression=no" -B 65536 --inplace --exclude='cache/' --exclude='*.tar' --exclude='tmp-*' root@168.119.98.135:/root/projects/windradabstand/data/ /Users/michaelkreil/Projekte/privat/ZSHH/windradabstand/data/ --delete-after
+#rsync -avhtWe "ssh" root@168.119.98.135:/root/projects/windradabstand/docs/tiles /Users/michaelkreil/Projekte/privat/ZSHH/windradabstand/docs/ --delete-after
