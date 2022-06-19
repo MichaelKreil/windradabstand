5e66aefa0bf850179fcb1a7084ffece8c1dfaabd
Michael Kreil <github@michael-kreil.de>
2022-06-19 18:27:28 +0200

    delete empty folders

--- bin/7_render_map-3_cleanup.sh
+++ bin/7_render_map-3_cleanup.sh
@@ -2,15 +2,19 @@
 cd "$(dirname "$0")"
 cd ../docs/tiles
 
+set -ex
 
-echo "1/4 cleanup"
+echo "1/5 cleanup"
 find . -mindepth 3 -maxdepth 3 -type d | shuf | parallel --progress --bar "md5sum {}/*.png | grep '91035755a501e30825fe66e5cc71f9ab' | cut -d' ' -f3- | xargs rm"
 
-echo "2/4 pngquant"
+echo "2/5 delete empty folders"
+find . -empty -type d -delete
+
+echo "3/5 pngquant"
 find . -mindepth 3 -maxdepth 3 -type d | shuf | parallel --progress --bar "pngquant -f --ext .png --quality=95-100 --speed 1 --strip {}/*.png"
 
-echo "3/4 optipng"
+echo "4/5 optipng"
 find . -mindepth 3 -maxdepth 3 -type d | shuf | parallel --progress --bar "optipng -quiet {}/*.png"
 
-echo "4/4 cleanup"
+echo "5/5 cleanup"
 find . -mindepth 3 -maxdepth 3 -type d | shuf | parallel --progress --bar "md5sum {}/*.png | grep '42cc2561c002c4e07aff64fc6f144241' | cut -d' ' -f3- | xargs rm"
