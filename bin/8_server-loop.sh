dc50095ca42b51c9c9d069cebccbf24567c8625f
Michael Kreil <github@michael-kreil.de>
2022-08-23 00:18:26 +0200

    improve code and logging

--- bin/8_server-loop.sh
+++ bin/8_server-loop.sh
@@ -4,6 +4,7 @@ cd "$(dirname "$0")"
 folder="../docs/tiles"
 myip="$(hostname -I | cut -d' ' -f1)"
 mainip="168.119.98.135"
+mainurl="http://${mainip}:8080/files"
 
 
 
@@ -11,11 +12,12 @@ echo "check germany.mbtiles"
 
 if [[ ! -f "${folder}/germany.mbtiles" ]]
 then
+	echo "   germany.mbtiles is missing. download."
 	if [ $myip == $mainip ]
 	then
 		wget -q --show-progress "https://storage.googleapis.com/datenhub-net-static/tiles/germany.mbtiles" -O "${folder}/germany.mbtiles.tmp"
 	else
-		wget -q --show-progress "http://${mainip}:8080/files/germany.mbtiles" -O "${folder}/germany.mbtiles.tmp"
+		wget -q --show-progress "${mainurl}/germany.mbtiles" -O "${folder}/germany.mbtiles.tmp"
 	fi
 	mv "${folder}/germany.mbtiles.tmp" "${folder}/germany.mbtiles"
 fi
@@ -34,14 +36,17 @@ then
 else
 	if [[ ! -f "${folder}/buffered.tar" ]]
 	then
-		wget -q --show-progress "http://${mainip}:8080/files/buffered.tar" -O "${folder}/buffered.tar.tmp"
+		echo "   buffered.tar is missing. download."
+		wget -q --show-progress "${mainurl}/buffered.tar" -O "${folder}/buffered.tar.tmp"
 		mv "${folder}/buffered.tar.tmp" "${folder}/buffered.tar"
 	else
 		filesize1="$(stat --printf='%s' '${folder}/buffered.tar')"
-		filesize2="$(curl -sI 'http://${mainip}:8080/files/buffered.tar') | grep -i Content-Length | awk '{print $2}'"
+		filesize2="$(curl -sI '${mainurl}/buffered.tar' | grep -i Content-Length | awk '{printf "%i",$2}')"
+		echo "   filesize1='${filesize1}', filesize2='${filesize2}'"
 		if [ $filesize1 != $filesize2 ]
 		then
-			wget -q --show-progress "http://${mainip}:8080/files/buffered.tar" -O "${folder}/buffered.tar.tmp"
+			echo "   buffered.tar is different. download again."
+			wget -q --show-progress "${mainurl}/buffered.tar" -O "${folder}/buffered.tar.tmp"
 			mv "${folder}/buffered.tar.tmp" "${folder}/buffered.tar"
 		fi
 	fi
