b146e369ef36ed510483774f6e65b316584c41c4
Michael Kreil <github@michael-kreil.de>
2022-08-25 17:19:25 +0200

    add seismic

---
+++ bin/4_process-9_seismic.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+'use strict'
+
+
+const fs = require('fs');
+const zlib = require('zlib');
+const helper = require('../lib/helper.js');
+const config = require('../config.js');
+const turf = require('@turf/turf');
+const { convertGeoJSON2Anything } = require('../lib/geohelper.js');
+
+start()
+
+async function start(opt) {
+	const filenameIn = config.getFilename.static('seismessstationen.tsv');
+	const ruleTypes = ['seismess'];
+	const filenameGeoJSON = config.getFilename.mapFeature('seismess.geojsonl.gz');
+	const filenameFGB = config.getFilename.mapFeature('seismess.fgb');
+
+	console.log('process seismess');
+
+	if (!fs.existsSync(filenameIn)) throw Error(filenameIn + ' is missing');
+	
+	ruleTypes.forEach(ruleType => {
+		if (!config.ruleTypes.find(t => t.slug === ruleType)) throw Error(`ruleType ${ruleType} is not defined in config.js`)
+	})
+
+	let stations = helper.readTSV(filenameIn, 'utf8');
+	let result = [];
+	for (let station of stations) {
+		station.lat = parseFloat(station.lat);
+		station.lng = parseFloat(station.lng);
+		station.radius = parseFloat(station.radius);
+
+		let feature = turf.buffer(turf.point([station.lng, station.lat]), station.radius / 1000);
+		feature.properties = station;
+		feature.properties.type = 'seismess';
+		feature.bbox = turf.bbox(feature);
+
+		result.push(JSON.stringify(feature)+'\n');
+	}
+	result = zlib.gzipSync(Buffer.from(result.join('')));
+	fs.writeFileSync(filenameGeoJSON, result);
+	
+	convertGeoJSON2Anything(filenameGeoJSON, filenameFGB);
+}
--- bin/4_process-all.sh
+++ bin/4_process-all.sh
@@ -5,4 +5,4 @@ set -ex
 node 4_process-1_siedlungsflaeche.js
 node 4_process-2_gebaeude.js
 
-parallel -j 80% "node 4_process-{}.js" ::: 3_grenze_flaeche 4_verkehrslinie 5_versorgungslinie 6_vegetation 7_gewaesserflaeche 8_verkehrsflaeche
+parallel "node 4_process-{}.js" ::: 3_grenze_flaeche 4_verkehrslinie 5_versorgungslinie 6_vegetation 7_gewaesserflaeche 8_verkehrsflaeche 9_seismic
