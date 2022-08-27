93ac69606159a440f6e5b4a6c41b4429600ac1c5
Michael Kreil <github@michael-kreil.de>
2022-08-27 14:47:06 +0200

    refactor

--- bin/6_buffered_geometry-1_prepare.js
+++ bin/6_buffered_geometry-1_prepare.js
@@ -4,30 +4,52 @@
 const config = require('../config.js');
 const { writeFileSync } = require('fs');
 const { getBundeslaender } = require('../lib/geohelper.js');
+const { ensureFolder } = require('../lib/helper.js');
+const { resolve } = require('path');
+const turf = require('@turf/turf');
 
-const bundeslaender = getBundeslaender();
 const { nabenhoehe, rotordurchmesser } = config.typicalWindTurbine;
 
-let ruleTypes = config.ruleTypes;
 
-for (let ruleType of ruleTypes) {
+
+let folderBundeslaender = config.getFilename.bufferedGeometry('_bundeslaender');
+ensureFolder(folderBundeslaender);
+let bundeslaender = getBundeslaender().map(bundesland => {
+	let filename = resolve(folderBundeslaender, bundesland.properties.ags + '.geojson');
+	writeFileSync(filename, JSON.stringify(turf.featureCollection(bundesland)))
+
+	bundesland.properties.bbox = turf.bbox(bundesland);
+	bundesland.properties.filename = filename;
+	return bundesland.properties;
+})
+
+
+
+let ruleTypes = config.ruleTypes.filter(ruleType => {
 	ruleType.filenameIn = config.getFilename.rulesGeoBasis(ruleType.slug + '.gpkg');
 	ruleType.folderOut = config.getFilename.bufferedGeometry(ruleType.slug);
 	ruleType.regions = [];
 
 	for (let bundesland of bundeslaender) {
-		let rules = config.rules.get(bundesland.properties.ags);
+		let rules = config.rules.get(bundesland.ags);
 		let func = rules[ruleType.slug];
 		if (!func) continue;
 
 		let radius = func(nabenhoehe, rotordurchmesser / 2);
 		ruleType.regions.push({
 			radius,
-			ags: bundesland.properties.ags,
+			ags: bundesland.ags,
+			filenameBase: resolve(ruleType.folderOut, '' + bundesland.ags),
 		})
 	}
-}
 
-ruleTypes = ruleTypes.filter(r => r.regions.length > 0)
+	if (ruleType.regions.length === 0) return false;
 
-writeFileSync(config.getFilename.bufferedGeometry('index.json'), JSON.stringify(ruleTypes, null, '\t'));
+	ensureFolder(ruleType.folderOut);
+	return true;
+})
+
+
+
+let result = { ruleTypes, bundeslaender }
+writeFileSync(config.getFilename.bufferedGeometry('index.json'), JSON.stringify(result, null, '\t'));
