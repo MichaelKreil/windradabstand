cc5f71ee29bf82dbf286b984fbc051166af1abb8
Michael Kreil <github@michael-kreil.de>
2022-08-28 22:42:57 +0200

    repair geometry

--- bin/2_get_alkis-3_generate_fgb.sh
+++ bin/2_get_alkis-3_generate_fgb.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 cd ../data/2_alkis/data
 set -ex
 
-ls -1S *.geojsonl.gz | parallel 'ogr2ogr -progress {= $_= substr $_, 0, -12 =}.fgb /vsigzip/{}'
+ls -1S *.geojsonl.gz | sed 's/.geojsonl.gz$//' | parallel 'rm -f {}.fgb; ogr2ogr -dialect SQLite -sql "SELECT ST_MakeValid(geometry) as geometry, * FROM \"{}.geojsonl\"" -nln {} {}.fgb /vsigzip/{}.geojsonl.gz; echo "created: {}.fgb"'
