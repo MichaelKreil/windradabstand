78c4509cdff0b40bbd153986ccb97ac6496926db
Michael Kreil <github@michael-kreil.de>
2022-08-29 08:58:52 +0200

    rename

--- bin/4_process-all.sh
+++ bin/4_process-all.sh
@@ -7,4 +7,4 @@ set -ex
 node 4_process-1_siedlungsflaeche.js
 node 4_process-2_gebaeude.js
 
-parallel "node 4_process-{}.js; echo '--------------------------------------------------'" ::: 3_grenze_flaeche 4_verkehrslinie 5_versorgungslinie 6_vegetation 7_gewaesserflaeche 8_verkehrsflaeche 9_seismic
+parallel "node 4_process-{}.js; echo '--------------------------------------------------'" ::: 3_grenze_flaeche 4_verkehrslinie 5_versorgungslinie 6_vegetation 7_gewaesserflaeche 8_verkehrsflaeche 9_seismisch
