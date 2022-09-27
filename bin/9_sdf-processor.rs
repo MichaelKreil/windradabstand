c9936e9d80966a364a796f674a4fcbbd7e7a3615
Michael Kreil <github@michael-kreil.de>
2022-09-27 23:35:36 +0200

    prepare geojson parser

--- bin/9_sdf-processor.rs
+++ bin/9_sdf-processor.rs
@@ -21,24 +21,33 @@ struct Arguments {
 	size: u32,
 }
 
+struct Point {
+	x: f64,
+	y: f64,
+}
+struct Ring(Vec<Point>);
+struct Polygon(Vec<Ring>);
+struct Collection(Vec<Polygon>);
+
 fn main() {
 	let arguments = parse_arguments();
 	println!("{:?}", arguments);
+	
+	let polygons = load_geometry(arguments.filename);
 	/*
-	let polygons = loadGeometry(arguments.filename);
 	let segments = getSegments(polygons);
-	let treePolygons = generateRTree(polygons);
-	let treeSegments = generateRTree(segments);
+	let rtreePolygons = generateRTree(polygons);
+	let ctreeSegments = generateRTree(segments);
 	let image = initializeImage();
 	for pixel in image {
 		let coords = pixel.getCoords();
-		let distance = getMinDistance(coords, treeSegments, coords);
+		let distance = getMinDistance(coords, ctreeSegments, coords);
 
 		if (distance > arguments.maxDistance) {
 			distance = arguments.maxDistance;
 		}
 
-		if (isInside(coords, treePolygons)) {
+		if (isInside(coords, rtreePolygons)) {
 			distance = -distance;
 		}
 
@@ -52,10 +61,14 @@ fn parse_arguments() -> Arguments {
 	let args: Vec<String> = env::args().collect();
 	return Arguments {
 		filename: args.get(2).unwrap_or(&"../data/9_sdf/test.geojson".to_string()).to_string(),
-		z:        args.get(3).unwrap_or(&"14".to_string()).parse().unwrap(),
-		y:        args.get(4).unwrap_or(&"0".to_string()).parse().unwrap(),
-		x:        args.get(5).unwrap_or(&"0".to_string()).parse().unwrap(),
-		n:        args.get(6).unwrap_or(&"16".to_string()).parse().unwrap(),
+		z:        args.get(3).unwrap_or( &"14".to_string()).parse().unwrap(),
+		y:        args.get(4).unwrap_or(  &"0".to_string()).parse().unwrap(),
+		x:        args.get(5).unwrap_or(  &"0".to_string()).parse().unwrap(),
+		n:        args.get(6).unwrap_or( &"16".to_string()).parse().unwrap(),
 		size:     args.get(7).unwrap_or(&"256".to_string()).parse().unwrap(),
 	};
+}
+
+fn load_geometry(filename:String) -> Collection {
+	return ();
 }
