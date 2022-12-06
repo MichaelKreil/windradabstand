70f72dffcfa0e4b4dca797e5015d40293e7ffeab
Michael Kreil <github@michael-kreil.de>
2022-12-06 11:55:40 +0100

    improve quality

--- bin/7_sdf-1_generate.js
+++ bin/7_sdf-1_generate.js
@@ -36,7 +36,7 @@ simpleCluster(async function (runWorker) {
 	let tilesTar = resolve(config.folders.tiles, 'tiles.tar');
 
 	console.log('1/3 pngquant')
-	await wrapExec(`cd "${pngFolder}"; find . -mindepth 2 -maxdepth 2 -type d | shuf | parallel --progress --bar "pngquant -f --ext .png --quality=70-80 --speed 1 --strip {}/*.png"`);
+	await wrapExec(`cd "${pngFolder}"; find . -mindepth 2 -maxdepth 2 -type d | shuf | parallel --progress --bar "pngquant -f --ext .png --quality=90-100 --speed 1 --strip {}/*.png"`);
 	
 	console.log('2/3 optipng')
 	await wrapExec(`cd "${pngFolder}"; find . -mindepth 2 -maxdepth 2 -type d | shuf | parallel --progress --bar "optipng -quiet {}/*.png"`);
@@ -45,6 +45,7 @@ simpleCluster(async function (runWorker) {
 	await wrapExec(`rm "${tilesTar}"; cd "${pngFolder}"; tar -cf "${tilesTar}" *`);
 
 	console.log('Finished');
+	return
 
 	async function processLevel(action, z) {
 		console.log(`process level ${z} using: ${action}`);
