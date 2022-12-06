7b31ca21e56f0ffa4d4ce8642b31efc429393246
Michael Kreil <github@michael-kreil.de>
2022-12-06 13:24:15 +0100

    remove compression

--- bin/7_sdf-1_generate.js
+++ bin/7_sdf-1_generate.js
@@ -32,18 +32,6 @@ simpleCluster(async function (runWorker) {
 	await processLevel('render', zoomLevel);
 	for (let z = zoomLevel - 1; z >= 0; z--) await processLevel('merge', z);
 
-	let pngFolder = resolve(config.folders.sdf, 'png');
-	let tilesTar = resolve(config.folders.tiles, 'tiles.tar');
-
-	console.log('1/3 pngquant')
-	await wrapExec(`cd "${pngFolder}"; find . -mindepth 2 -maxdepth 2 -type d | shuf | parallel --progress --bar "pngquant -f --ext .png --quality=90-100 --speed 1 --strip {}/*.png"`);
-	
-	console.log('2/3 optipng')
-	await wrapExec(`cd "${pngFolder}"; find . -mindepth 2 -maxdepth 2 -type d | shuf | parallel --progress --bar "optipng -quiet {}/*.png"`);
-
-	console.log('3/3 tar')
-	await wrapExec(`rm "${tilesTar}"; cd "${pngFolder}"; tar -cf "${tilesTar}" *`);
-
 	console.log('Finished');
 	return
 
