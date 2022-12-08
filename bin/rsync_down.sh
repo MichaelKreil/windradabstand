5a6e8e7fe65f5052511d2eb0f43bddcc1994b9a9
Michael Kreil <github@michael-kreil.de>
2022-12-08 23:04:25 +0100

    cleanup

--- bin/rsync_down.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-set -ex
-
-rsync -azxvhtPSe "ssh -o Compression=no" -B 65536 --inplace --exclude='cache/' --exclude='tmp-*' root@168.119.98.135:/root/projects/windradabstand/data/ /Users/michaelkreil/Projekte/privat/ZSHH/windradabstand/data/ --delete-after
-rsync -avhtWe "ssh" root@168.119.98.135:/root/projects/windradabstand/docs/tiles /Users/michaelkreil/Projekte/privat/ZSHH/windradabstand/docs/ --delete-after
